60c94891017c5990a283c631fe525a07cb19f92d
michael <zhangxiaocong_2017@126.com>
2018-12-29 22:15:55 +0800

    修改空投

--- airdropself.js
+++ airdropself.js
@@ -39,8 +39,9 @@ options = {
 //     })
 //
 // });
-eos.transfer('godapp.e', "zhangaccount", '0.0001 EOS', 'You HAVE know what GoDapp is if you\'re an EOS loyal supporter. Our website: godapp.com\n' +
-    '如果你是eos的忠实支持者，最好了解一下godapp是什么，我们的官网 godapp.com', options).catch(results=>{
+// eos.transfer('godapp.e', "bombdesigner", '15.80 EOS', '您好，受eos网络影响导致的吞币，我们已返还给您，如有疑问请通过godapp.com官网所留的联系方式联系我们. ', options).catch(results=>{
+//     console.log("err"+results);
+eos.transfer('godapp.e', "bombdesigner", '15.8000 EOS', '上传dapp,获取获胜截图,返还  ', options).catch(results=>{
     console.log("err"+results);
     //随眠一秒
     // for (let i = 0; i <3 ; i++) {
