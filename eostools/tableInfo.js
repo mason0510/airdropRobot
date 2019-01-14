b8ba12058702b07b3a23dbb72337a75c964d3963
Michael <zhangxiaocong_2011@yeah.net>
2019-01-14 22:05:46 +0800

    删去了多余的网络请求

--- eostools/tableInfo.js
+++ eostools/tableInfo.js
@@ -1,9 +1,37 @@
 let request= require('request');
+let constants=require('../utils/constants');
 //获取当前的轮次
 getGameTable=async ()=>{
     // console.log("==============");
     let promise=new Promise(async resolve => {
-        await request.post('https://proxy.eosnode.tools/v1/chain/get_table_rows', {
+          // await process.on("err",async()=>{
+               await request.post(constants.url1+'/v1/chain/get_table_rows', {
+                   json: {
+                       code: 'warofstar.e',
+                       table:"gametable",
+                       scope:"warofstar.e",
+                       json:true
+                   }
+               }, async (error, res, body) => {
+                   if (error) {
+                       console.error(error)
+                       return
+                   }
+                   console.log(`statusCode: ${res.statusCode}`);
+                   // let gamestatus =await body.rows[0].status;
+                   // let end_time=await body.rows[0].end_time;
+                   // let roundId=await body.rows[0].id;
+                   return resolve(body);
+               }).on("error", function(error) {
+                   console.log("Error: Unable to get " + url, "#ff0000");
+                   console.log(error, "#ff0000");
+                   callback(null);
+               });
+           // })
+
+    }).catch(async error=>{
+        console.log(error);
+        await request.post(constants.url2+'/v1/chain/get_table_rows', {
             json: {
                 code: 'warofstar.e',
                 table:"gametable",
@@ -16,11 +44,15 @@ getGameTable=async ()=>{
                 return
             }
             console.log(`statusCode: ${res.statusCode}`);
-           // let gamestatus =await body.rows[0].status;
-           // let end_time=await body.rows[0].end_time;
-           // let roundId=await body.rows[0].id;
+            // let gamestatus =await body.rows[0].status;
+            // let end_time=await body.rows[0].end_time;
+            // let roundId=await body.rows[0].id;
             return resolve(body);
-        })
+        }).on("error", function(error) {
+            console.log("Error: Unable to get " + url, "#ff0000");
+            console.log(error, "#ff0000");
+            callback(null);
+        });
     });
     return promise;
 };
@@ -28,32 +60,44 @@ getGameTable=async ()=>{
 getPlayerTable=async ()=>{
     // console.log("==============");
     let promise=new Promise(async resolve => {
-        await request.post('https://proxy.eosnode.tools/v1/chain/get_table_rows', {
+   // await  process.on("err", async () => {
+         await request.post(constants.url1+'/v1/chain/get_table_rows', {
+             json: {
+                 code: 'warofstar.e',
+                 table: "bets",
+                 scope: "warofstar.e",
+                 json: true
+             }
+         }, async (error, res, body) => {
+             if (error) {
+                 console.error(error)
+                 return
+             }
+             console.log(`statusCode: ${res.statusCode}`);
+             return resolve(body);
+         })
+        }).catch(async error=>{
+        await request.post(constants.url2+'/v1/chain/get_table_rows', {
             json: {
                 code: 'warofstar.e',
-                table:"bets",
-                scope:"warofstar.e",
-                json:true
+                table: "bets",
+                scope: "warofstar.e",
+                json: true
             }
         }, async (error, res, body) => {
             if (error) {
                 console.error(error)
                 return
             }
-            // [ { id: 33305,
-            //     game_id: 7748,
-            //     player: 'yiyiranranfc',
-            //     referer: 'houseaccount',
-            //     bet: '0.5000 EOS',
-            //     bet_type: 2 } ],
             console.log(`statusCode: ${res.statusCode}`);
-            // let gamestatus =await body.rows[0].status;
-            // let end_time=await body.rows[0].end_time;
-            // let roundId=await body.rows[0].id;
-            //console.log(body.rows[0].player);
             return resolve(body);
-        })
+        }).on("error", function(error) {
+            console.log("Error: Unable to get " + url, "#ff0000");
+            console.log(error, "#ff0000");
+            callback(null);
+        });
     });
+
     return promise;
 };
 
@@ -61,48 +105,82 @@ getPlayerTable=async ()=>{
 getPlayerTable=async ()=>{
     // console.log("==============");
     let promise=new Promise(async resolve => {
-        await request.post('https://proxy.eosnode.tools/v1/chain/get_table_rows', {
-            json: {
-                code: 'warofstar.e',
-                table:"bets",
-                scope:"warofstar.e",
-                json:true
-            }
-        }, async (error, res, body) => {
-            if (error) {
-                console.error(error)
-                return
-            }
-            // [ { id: 33305,
-            //     game_id: 7748,
-            //     player: 'yiyiranranfc',
-            //     referer: 'houseaccount',
-            //     bet: '0.5000 EOS',
-            //     bet_type: 2 } ],
-            console.log(`statusCode: ${res.statusCode}`);
-            // let gamestatus =await body.rows[0].status;
-            // let end_time=await body.rows[0].end_time;
-            // let roundId=await body.rows[0].id;
-            //console.log(body.rows[0].player);
-            return resolve(body);
-        })
-    });
+       // await process.on("err", async () => {
+            await request.post(constants.url1+'/v1/chain/get_table_rows', {
+                json: {
+                    code: 'warofstar.e',
+                    table: "bets",
+                    scope: "warofstar.e",
+                    json: true
+                }
+            }, async (error, res, body) => {
+                if (error) {
+                    console.error(error)
+                    return
+                }
+                // [ { id: 33305,
+                //     game_id: 7748,
+                //     player: 'yiyiranranfc',
+                //     referer: 'houseaccount',
+                //     bet: '0.5000 EOS',
+                //     bet_type: 2 } ],
+                console.log(`statusCode: ${res.statusCode}`);
+                // let gamestatus =await body.rows[0].status;
+                // let end_time=await body.rows[0].end_time;
+                // let roundId=await body.rows[0].id;
+                //console.log(body.rows[0].player);
+                return resolve(body);
+            }).on("error", function(error) {
+                console.log("Error: Unable to get " + url, "#ff0000");
+                console.log(error, "#ff0000");
+                callback(null);
+            });
+
+
+        // });
+    }).catch(error=>{
+        console.log(error);
+    })
     return promise;
 };
 
 getPlayerInComeTable=async ()=>{
     // console.log("==============");
     let promise=new Promise(async resolve => {
-        await request.post('https://proxy.eosnode.tools/v1/chain/get_table_rows', {
+            await request.post(constants.url1+'/v1/chain/get_table_rows', {
+                json: {
+                    code: 'warofstar.e',
+                    table: "bets",
+                    scope: "warofstar.e",
+                    json: true
+                }
+            }, async (error, res, body) => {
+                if (error) {
+                    return
+                }
+                // [ { id: 33305,
+                //     game_id: 7748,
+                //     player: 'yiyiranranfc',
+                //     referer: 'houseaccount',
+                //     bet: '0.5000 EOS',
+                //     bet_type: 2 } ],
+                console.log(`statusCode: ${res.statusCode}`);
+                // let gamestatus =await body.rows[0].status;
+                // let end_time=await body.rows[0].end_time;
+                // let roundId=await body.rows[0].id;
+                //console.log(body.rows[0].player);
+                return resolve(body);
+            })
+        }).catch(async error=>{
+        await request.post(constants.url2+'/v1/chain/get_table_rows', {
             json: {
                 code: 'warofstar.e',
-                table:"bets",
-                scope:"warofstar.e",
-                json:true
+                table: "bets",
+                scope: "warofstar.e",
+                json: true
             }
         }, async (error, res, body) => {
             if (error) {
-                console.error(error)
                 return
             }
             // [ { id: 33305,
@@ -117,27 +195,31 @@ getPlayerInComeTable=async ()=>{
             // let roundId=await body.rows[0].id;
             //console.log(body.rows[0].player);
             return resolve(body);
-        })
+        }).on("error", function(error) {
+            console.log("Error: Unable to get " + url, "#ff0000");
+            console.log(error, "#ff0000");
+            callback(null);
+        });
     });
     return promise;
 };
 
 
-module.exports={getGameTable,getPlayerTable};
+module.exports={getGameTable,getPlayerTable,getPlayerInComeTable};
 
-let body=async()=>{
-    try {
-        let body=await getGameTable();
-        if (body.rows.length!==0){
-            // console.log(body.rows[0].player)
-            //   let player1=body.rows[0].player;
-            console.log("结束时间"+body.rows[0].end_time);
-            console.log("当前时间"+parseInt(Date.now()/1000));
-        }
-    }catch (e) {
-        console.log(e)
-    }
-
-
-}
-body()
+// let body=async()=>{
+//     try {
+//         let body=await getGameTable();
+//         if (body.rows.length!==0){
+//             // console.log(body.rows[0].player)
+//             //   let player1=body.rows[0].player;
+//             console.log("结束时间"+body.rows[0].end_time);
+//             console.log("当前时间"+parseInt(Date.now()/1000));
+//         }
+//     }catch (e) {
+//         console.log(e)
+//     }
+//
+//
+// }
+// body()
